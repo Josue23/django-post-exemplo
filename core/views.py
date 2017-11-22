4c1f19916aed4feae5e603921b39be838e8e11c2
josue <josue@jobconvo.com>
2017-11-22 12:42:56 -0200

    novos templates

--- core/views.py
+++ core/views.py
@@ -6,55 +6,84 @@ from .forms import AlunoForm, ProfessorForm, CourseForm
 from django.views import generic
 
 
-
 def index(request):
-	return render(request, 'index.html')
+    return render(request, 'index.html')
 
-def aluno_add(request):
-	if request.method == 'POST':
-		alunoform = AlunoForm(request.POST)
-		if alunoform.is_valid:
-			alunoform.save()
-			# import ipdb; ipdb.set_trace()
-		return redirect('aluno_list')
-	else: 
-		alunoform = AlunoForm()
-	ctx = {'aluno_form': alunoform}
-	return render(request, 'aluno_add.html', ctx)
 
 def cadastro(request):
-	if request.method == 'POST':
+    if request.method == 'POST':
 
-		formAluno = AlunoForm(request.POST)
-		if 'enviarAluno' in request.POST and formAluno.is_valid():
-			formAluno.save()
-			# print 'success'
+        formAluno = AlunoForm(request.POST)
+        if 'enviarAluno' in request.POST and formAluno.is_valid():
+            formAluno.save()
+            # print 'success'
 
-		formProfessor = ProfessorForm(request.POST)
-		if 'enviarProfessor' in request.POST and formProfessor.is_valid():
-			formProfessor.save()
+        formProfessor = ProfessorForm(request.POST)
+        if 'enviarProfessor' in request.POST and formProfessor.is_valid():
+            formProfessor.save()
 
-		formCourse = CourseForm(request.POST)
-		if 'enviarCourse' in request.POST and formCourse.is_valid():
-			formCourse.save()
+        formCourse = CourseForm(request.POST)
+        if 'enviarCourse' in request.POST and formCourse.is_valid():
+            formCourse.save()
 
-	else:
-		formAluno = AlunoForm()
-		formProfessor = ProfessorForm()
-		formCourse = CourseForm()
+    else:
+        formAluno = AlunoForm()
+        formProfessor = ProfessorForm()
+        formCourse = CourseForm()
 
-	context = {'formAluno': formAluno, 'formProfessor': formProfessor, 'formCourse': formCourse}
+    context = {'formAluno': formAluno,
+               'formProfessor': formProfessor, 'formCourse': formCourse}
 
-	return render(request, 'home.html', context)
+    return render(request, 'home.html', context)
 
-	
+
+def aluno_add(request):
+    if request.method == 'POST':
+        alunoform = AlunoForm(request.POST)
+        if alunoform.is_valid:
+            alunoform.save()
+            # import ipdb; ipdb.set_trace()
+        return redirect('aluno_list')
+    else:
+        alunoform = AlunoForm()
+    ctx = {'aluno_form': alunoform}
+    return render(request, 'core/aluno_add.html', ctx)
+
+
 def aluno_list(request):
-	aluno_list = Aluno.objects.all()
-	ctx = {'aluno_list': aluno_list}
-	return render(request, 'aluno_list.html', ctx)
+    aluno_list = Aluno.objects.all()
+    ctx = {'aluno_list': aluno_list}
+    return render(request, 'core/aluno_list.html', ctx)
 
 
 def aluno_detail(request, pk):
-	aluno = Aluno.objects.get(pk=pk)
-	ctx = {'aluno': aluno}
-	return render(request, 'aluno_detail.html', ctx)
+    aluno = Aluno.objects.get(pk=pk)
+    ctx = {'aluno': aluno}
+    return render(request, 'core/aluno_detail.html', ctx)
+
+
+def aluno_edit(request, pk):
+    if request.is_ajax() and request.method == 'POST':
+        pk = request.POST.get('pk')
+        aluno = get_object_or_404(Candidate, pk=pk)
+        aluno.nome = request.POST.get('nome')
+        aluno.sobrenome = request.POST.get('sobrenome')
+        candidate.email = request.POST.get('email')
+
+        aluno.save()
+        response = {'status': 'update'}
+        return JsonResponse(response)
+    else:
+        return HttpResponseRedirect(reverse('aluno_list'))
+
+
+def professor_list(request):
+    professor_list = Professor.objects.all()
+    ctx = {'professor_list': professor_list}
+    return render(request, 'core/professor_list.html', ctx)
+
+
+def professor_detail(request, pk):
+    professor = Professor.objects.get(pk=pk)
+    ctx = {'professor': professor}
+    return render(request, 'core/professor_detail.html', ctx)
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,13 +1,26 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from core.views import cadastro, aluno_add, index, aluno_list, aluno_detail
+from core import views as v
+from django.core.urlresolvers import reverse
 
 
 urlpatterns = [
-    url(r'^$', index, name='index'),
-    url(r'^aluno/add/$', aluno_add, name='aluno_add'),
-    url(r'^aluno_list/$', aluno_list, name='aluno_list'),
-    url(r'^add/$', cadastro, name='cadastro'),
-    url(r'^aluno/(?P<pk>\d+)/$', aluno_detail, name='aluno_detail'),
+    url(r'^$', v.index, name='index'),
+
+    # aluno
+    url(r'^aluno/add/$', v.aluno_add, name='aluno_add'),
+    url(r'^aluno_list/$', v.aluno_list, name='aluno_list'),
+    url(r'^aluno/(?P<pk>\d+)/$', v.aluno_detail, name='aluno_detail'),
+    url(r'^aluno/(?P<pk>\d+)/edit/$', v.aluno_edit, name='aluno_edit'),
+    # url(r'^candidate/(?P<pk>\d+)/edit/$', v.candidate_edit, name='candidate_edit'),
+    
+    # professor
+    url(r'^professor_list/$', v.professor_list, name='professor_list'),
+    url(r'^professor/(?P<pk>\d+)/$', v.professor_detail, name='professor_detail'),
+
+    # cadastro com 3 forms
+    url(r'^add/$', v.cadastro, name='cadastro'),
+    
+    # admin
     url(r'^admin/', include(admin.site.urls)),
 ]
